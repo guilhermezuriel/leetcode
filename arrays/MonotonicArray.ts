e5ca79fa2523782769cf86aa745201f9f0c9e905
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-11-10 17:01:11 -0300

    Monotonic Arrays Solution

---
+++ arrays/MonotonicArray.ts
@@ -0,0 +1,55 @@
+function isMonotonic(nums: number[]): boolean {
+  const n = nums.length;
+  const last = n - 1;
+
+  if (n <= 1) {
+    return true;
+  }
+
+  let pointer = 0;
+  let i = 1;
+
+  if (nums[pointer] === nums[last]) {
+    while (i != n) {
+      if (nums[i] != nums[pointer]) {
+        return false;
+      }
+      i++;
+      pointer++;
+    }
+  }
+
+  if (nums[pointer] < nums[last]) {
+    while (i != n) {
+      if (nums[pointer] > nums[i]) {
+        return false;
+      }
+      i++;
+      pointer++;
+    }
+  }
+
+  if (nums[pointer] > nums[last]) {
+    while (i != n) {
+      if (nums[pointer] < nums[i]) {
+        return false;
+      }
+      i++;
+      pointer++;
+    }
+  }
+  return true;
+}
+
+function main() {
+  let testes = [
+    [1, 2, 2, 3],
+    [6, 5, 4, 4],
+    [1, 3, 2],
+  ];
+  for (let teste of testes) {
+    console.log(isMonotonic(teste));
+  }
+}
+
+main();
